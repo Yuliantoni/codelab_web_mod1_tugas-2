1168561bbfa8d79ad01717e39ed08abf9eb31887
Yuliantoni <148980670+Yuliantoni@users.noreply.github.com>
2024-12-31 10:13:55 +0700

    Add files via upload

---
+++ praktikum web_2/script_prak_tugas2.js
@@ -0,0 +1,26 @@
+let todoList = document.getElementById('todo-list');
+
+// Fungsi untuk menambah tugas ke daftar
+function addTask() {
+    let taskInput = document.getElementById('todo-input');
+    let task = taskInput.value;
+
+    if (task.trim() !== "") {
+        // Membuat elemen list baru
+        let li = document.createElement('li');
+        li.innerHTML = `<span>${task}</span> <button class="delete-btn" onclick="deleteTask(this)">Delete</button>`;
+       
+        todoList.appendChild(li);
+
+      
+        taskInput.value = '';
+    } else {
+        alert("Please enter a task.");
+    }
+}
+
+// Fungsi untuk menghapus tugas dari daftar
+function deleteTask(button) {
+    let li = button.parentElement;
+    todoList.removeChild(li);
+}
